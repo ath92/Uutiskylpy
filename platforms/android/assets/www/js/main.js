09599b0445e4f7a3d2a5b62a784c8154c28ba4c4
unknown <tom@ath92.nl>
2016-05-02 16:27:45 +0300

    better tooltips

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -1,16 +1,10 @@
 $(document).ready(function(){
 
 	var sentences, sentenceStart, sentenceEnd, currentSentence;//global variables that contains the HTML for each sentence and two vars for end and beginning of current sentence (in seconds)
-
-	console.log('hello world!');
-
-	var colors=["#f99","#9f9", "#99f", "#ff9", "#f9f", "#9ff", "#caf", "#fca"];
+	var colors=["#ff5f5f", "#8b95f4", "#fff8a0", "#48fe99", "#898989", "#f48be7", "#e0750c", "#abfff7", "#75b090", "#bd8bf4", "#cfcfcf", "#edae53", "#8bd0f4", "#bc8a8a", "#b0f781", "#ff45c9"];
 
 	var video = document.getElementById('video');
 	video.play();
-	$('#video').resize(function(){
-		//$('#content').css({'height': ($(window).height() - $('#video').height()) + 'px'});//-20 is a dirty thing but I don't feel like changing it right now
-	});
 
 	video.addEventListener("timeupdate", function(){
 	    if(this.currentTime >= sentenceEnd) {
@@ -28,8 +22,11 @@ $(document).ready(function(){
 		    
 			//we start by splitting the incoming data (the pseudo xml) into seperate sentences.
 
-	    	var newdata = data.replace('</mw-sentence>', '</mw-sentence>}}{{');//add delimiters
+	    	var newdata = data.replaceAll('</mw-sentence>', '</mw-sentence>}}{{');//add delimiters
 	    	sentences = newdata.split('}}{{');
+	    	if(sentences[sentences.length-1]=="") sentences.pop(); // for some reason, there is an empty string at the end of the array.
+
+	    	console.log(sentences);
 
 	    	//then, we add the first sentence to the body.
 
@@ -38,10 +35,10 @@ $(document).ready(function(){
 	    	//now let's also add the timeline buttons
 
 	    	for(var i = 0; i < sentences.length; i++){
-	    		var element = $('<a href="#">'+i+'</a>');
+	    		var element = $('<a href="#" sentece-id="'+i+'"></a>');
 	    		if(i==0) element.addClass('active');
 	    		element.on('click', function(){
-	    			setSentence($(this).html());
+	    			setSentence($(this).attr('sentece-id'));
 	    		});
 	    		$('#timeline').append(element);
 	    	}
@@ -65,26 +62,24 @@ $(document).ready(function(){
 
     			video.currentTime = sentenceStart;
     			video.play();
-
+    			$('#timeline a.active').addClass('complete');
     			$('#timeline a').removeClass('active');
     			$('#timeline a:nth-child('+(parseInt(sentenceId)+1)+')').addClass('active');
 
-    			console.log(sentenceId, $('#timeline a:nth-child('+(parseInt(sentenceId)+1)+')'));
-
-
-
 		    	$('mw-finnish mw-word').on('click', function(e){
-		    		$('mw-sentence, mw-word').removeClass('active');
+		    		$('#tooltip').addClass('active');
+		    		$('mw-word, mw-sentence').removeClass('active');
 		    		$(this).addClass('active');
+		    		$('#tooltip').html($(this).parent().parent().find('mw-tooltip[finnish-id="'+$(this).attr('id')+'"]').prop('outerHTML'));
 		    		clearTimeout(timeout);
 		    		timeout = setTimeout(function(){
-		    			$('mw-word').removeClass('active');
+		    			$('#tooltip, mw-word').removeClass('active');
 		    		}, 2000);
 		    	});
 
 
 		    	$('mw-finnish').on('dblclick', function(e){
-		    		$('mw-word').removeClass('active');
+		    		$('mw-word, #tooltip').removeClass('active');
 		    		$('mw-sentence').toggleClass('active');
 		    	});
 
@@ -100,14 +95,36 @@ $(document).ready(function(){
 
 		    		$(this).css({'border': '2px solid ' + colors[iterator%colors.length]});
 					$('mw-arabic mw-word[finnish-id="'+finnishId+'"]').css({'background-color':colors[iterator%colors.length]});
-		    		var arabicWords = $('mw-arabic mw-word[finnish-id="'+finnishId+'"]');
-		    		console.log(arabicWords);
-		    		var arabicHTMLString = "";
-		    		for(i=0;i<arabicWords.length;i++){
-		    			arabicHTMLString += arabicWords.get(i).innerHTML;
-		    		}
 
-		    		$(this).append('<mw-tooltip>'+arabicHTMLString+'</mw-tooltip>');
+					var sentence = $(this).parent().parent();// mw-word --> mw-finnish -> mw-sentece
+
+					//dynamically add tooltip if not already there in the xml. Assumes there is one word in finnish that directly translates to all the corresponding arabic words, without caring about grammar etc.
+					if(sentence.find('mw-tooltip[finnish-id="'+finnishId+'"]').length == 0)
+					{
+			    		var arabicWords = $('mw-arabic mw-word[finnish-id="'+finnishId+'"]');
+			    		
+			    		var arabicHTMLString = "";
+			    		for(i=0;i<arabicWords.length;i++){
+			    			arabicHTMLString += arabicWords.get(i).innerHTML;
+			    		}
+			    		var $tooltip = $('<mw-tooltip finnish-id="'+finnishId+'"><finnish-part id="1">'+$(this).html()+'</finnish-part><arabic-part finnish-id="1">'+arabicHTMLString+'</arabic-part></mw-tooltip>'); 
+
+			    		sentence.find('mw-tooltips').append($tooltip);
+					} else {
+						$tooltip = sentence.find('mw-tooltip[finnish-id="'+finnishId+'"]');
+
+						var finnishParts = $tooltip.find('finnish-part');
+						console.log(finnishParts);
+			    		if(finnishParts.length > 1){
+			    			var colorIterator = 0;
+			    			$tooltip.find('finnish-part').each(function(e){
+			    				$(this).css({'background-color':colors[colorIterator%colors.length]});
+			    				if(!$(this).is(':last-of-type')) $(this).after(' + ');
+			    				$tooltip.find('arabic-part[finnish-id="'+$(this).attr('id')+'"]').css({'background-color':colors[colorIterator%colors.length]});
+			    				colorIterator++;
+			    			});
+			    		}
+					}
 
 		    		iterator++;
 		    	});
@@ -119,3 +136,8 @@ $(document).ready(function(){
     });
 
 });
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
